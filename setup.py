522919657a205acd9b3c96a1af1ed98e53b02918
dhilipsiva <dhilipsiva@pm.me>
2019-08-07 14:24:33 +0530

    Package library

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+__VERSION__ = '0.0.0'
+
+setup(
+    name='rsnsfw',
+    version=__VERSION__,
+    description="Reckonsys NSFW fork",  # NOQA
+    long_description="NSFW detection",
+    url='https://github.com/reckonsys/rsnsfw',
+    author='rockyzhengwu',
+    author_email='zhengwu@midday.me',
+    maintainer='reckonsys',
+    maintainer_email='info@reckonsys.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    keywords='reckonsys nsfw',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow',
+    ],
+)
